255c5b3db8f3fee463bc5f178f949aaabbe4bc5a
Floyd Kretschmar <floyd.kretschmar@otto.de>
2025-05-25 23:45:30 +0900

    Implement the basic version of voice transcription using whisper

--- src/main.py
+++ src/main.py
@@ -1,44 +1,70 @@
 import configparser
-import numpy
+import threading
 from configparser import ConfigParser
-from queue import Queue
+from queue import Queue, Empty
 from threading import Thread
-from vad_recorder import VADRecorder, SamplingRate
+
+import numpy
+
 from transcriber import Transcriber
+from vad_recorder import VADRecorder, SamplingRate
+
 
 def get_windowed_audio_chunks_in_order(audio_chunks: list[bytes], starting_index: int) -> bytes:
     """
     Returns the audio chunks in the order they were recorded, starting from the given index.
+    :param audio_chunks: List of audio chunks
+    :param starting_index: Index to start from
     """
     return b''.join(audio_chunks[starting_index:] + audio_chunks[:starting_index])
 
 def calculate_window_size(rate: SamplingRate, window_ms: int) -> int:
     """
     Calculate the number of chunks needed for the evaluation window based on the sampling rate.
+    :param rate: SamplingRate enum value (LOW or HIGH)
+    :param window_ms: Window size in milliseconds
     """
     chunk_size_in_ms: int = int((rate.get_chunk_size() / rate.value) * 1000)
     return window_ms // chunk_size_in_ms
 
-def transcribe_audio(transcriber: Transcriber, transcription_queue: Queue[bytes]) -> None:
+def transcribe_audio(transcriber: Transcriber, language: str, transcription_queue: Queue[bytes], cancellation_event: threading.Event) -> None:
     """
     Function to transcribe audio from the queue.
     This function is intended to be run in a separate thread.
+
+    :param transcriber: Transcriber instance to use for transcription
+    :param language: Language code for the transcription (e.g., 'en' for English)
+    :param transcription_queue: Queue containing audio data to be transcribed
+    :param cancellation_event: Event to signal cancellation of the transcription process
     """
     # Placeholder for transcription logic
-    while True:
-        print(transcription_queue.get())
-        transcription_queue.task_done()
-        print("Transcribing audio")
+    while not cancellation_event.is_set():
+        try:
+            audio: bytes = transcription_queue.get(timeout=1)
+        except Empty:
+            continue
+        transcribed_audio: str = transcriber.transcribe(
+            audio,
+            language=language)
+        print(transcribed_audio)
 
 def evaluate_audio(
         sampling_rate: SamplingRate,
         window_size: int,
         activation_threshold: float,
         deactivation_threshold: float,
-        transcription_queue: Queue[bytes]) -> None:
+        transcription_queue: Queue[bytes],
+        cancellation_event: threading.Event) -> None:
     """
     Function to record audio using the VADRecorder.
     This function is intended to be run in a separate thread.
+
+    :param sampling_rate: SamplingRate enum value (LOW or HIGH)
+    :param window_size: Number of audio chunks to consider for the evaluation window
+    :param activation_threshold: Threshold for activating audio collection
+    :param deactivation_threshold: Threshold for deactivating audio collection
+    :param transcription_queue: Queue to put audio data for transcription
+    :param cancellation_event: Event to signal cancellation of the recording process
     """
     recorder: VADRecorder = VADRecorder(
         sampling_rate,
@@ -51,6 +77,8 @@ def evaluate_audio(
     windowed_audio_chunks: list[bytes] = [bytes() for _ in range(window_size)]
 
     for audio, confidence in recorder.start():
+        if cancellation_event.is_set():
+            break
         confidence_window[window_index] = confidence
         windowed_audio_chunks[window_index] = audio
 
@@ -86,23 +114,27 @@ if __name__ == "__main__":
     act_threshold: float = parser.getfloat("VOICE_ACTIVATION", "activation_threshold", fallback=0.9)
     deact_threshold: float = parser.getfloat("VOICE_ACTIVATION", "deactivation_threshold", fallback=0.2)
     num_transcriber_threads: int = parser.getint("TRANSCRIPTION", "number_of_transcriber_threads", fallback=1)
+    lan: str = parser.getint("TRANSCRIPTION", "language", fallback="en")
 
     trans: Transcriber = Transcriber(
         model_name=parser.get("TRANSCRIPTION", "model_name", fallback="tiny"),
         device_type=parser.get("TRANSCRIPTION", "device_type", fallback="cpu"),
         number_of_threads=parser.getint("TRANSCRIPTION", "number_of_whisper_threads", fallback=1))
 
-    producer = Thread(target=evaluate_audio, args=(rate, ws, act_threshold, deact_threshold, queue))
+    stop_event: threading.Event = threading.Event()
+    producer = Thread(target=evaluate_audio, args=(rate, ws, act_threshold, deact_threshold, queue, stop_event))
     producer.start()
 
     threads = [producer]
     for _ in range(num_transcriber_threads):
-        transcriber_thread = Thread(target=transcribe_audio, args=(trans, queue))
+        transcriber_thread = Thread(target=transcribe_audio, args=(trans, lan, queue, stop_event))
         threads.append(transcriber_thread)
         transcriber_thread.start()
-
     try:
         for thread in threads:
             thread.join()
-    except KeyboardInterrupt:
+    except (SystemExit, KeyboardInterrupt) as e:
         print("Exiting...")
+        stop_event.set()
+        for thread in threads:
+            thread.join()
--- src/transcriber.py
+++ src/transcriber.py
@@ -1,5 +1,8 @@
+import numpy
 from faster_whisper import WhisperModel
-from configparser import ConfigParser
+
+from utils import audio_to_float
+
 
 class Transcriber:
     def __init__(self, model_name: str, device_type: str, number_of_threads: int):
@@ -11,3 +14,22 @@ class Transcriber:
             cpu_threads=number_of_threads,
             local_files_only=False)
         print("Transcriber initialized")
+
+    def transcribe(self, audio: bytes, language: str) -> str:
+        """
+        Transcribe the given audio bytes to text.
+
+        :param language: Language code for the transcription (e.g., 'en' for English)
+        :param audio: Audio data in bytes format
+        :return: Transcribed text
+        """
+        audio_data_array: numpy.ndarray = audio_to_float(audio)
+        segments, _ = self.whisper.transcribe(audio_data_array,
+                                language=language,
+                                beam_size=5,
+                                vad_filter=True,
+                                vad_parameters=dict(min_silence_duration_ms=1000))
+
+        segments = [s.text for s in segments]
+        transcription = " ".join(segments)
+        return transcription
