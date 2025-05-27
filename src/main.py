d1701059af9b605cb5fda5eadadac755dcdbd3cf
Floyd Kretschmar <fkretschmar@googlemail.com>
2025-05-27 21:49:40 +0900

    Small refactor

--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ import numpy
 from transcriber import Transcriber
 from vad_recorder import VADRecorder, SamplingRate
 
-
 def get_windowed_audio_chunks_in_order(audio_chunks: list[bytes], starting_index: int) -> bytes:
     """
     Returns the audio chunks in the order they were recorded, starting from the given index.
@@ -27,47 +26,49 @@ def calculate_window_size(rate: SamplingRate, window_ms: int) -> int:
     chunk_size_in_ms: int = int((rate.get_chunk_size() / rate.value) * 1000)
     return window_ms // chunk_size_in_ms
 
-def transcribe_audio(transcriber: Transcriber, language: str, transcription_queue: Queue[bytes], cancellation_event: threading.Event) -> None:
+def transcribe_audio(transcriber: Transcriber, language: str, audio_to_transcribe_queue: Queue[bytes], cancellation_event: threading.Event) -> None:
     """
     Function to transcribe audio from the queue.
     This function is intended to be run in a separate thread.
 
-    :param transcriber: Transcriber instance to use for transcription
+    :param transcriber: Transcriber instance to perform the transcription
     :param language: Language code for the transcription (e.g., 'en' for English)
-    :param transcription_queue: Queue containing audio data to be transcribed
+    :param audio_to_transcribe_queue: Queue containing audio data to be transcribed
     :param cancellation_event: Event to signal cancellation of the transcription process
     """
-    # Placeholder for transcription logic
+
     while not cancellation_event.is_set():
         try:
-            audio: bytes = transcription_queue.get(timeout=1)
+            audio: bytes = audio_to_transcribe_queue.get(timeout=1)
         except Empty:
             continue
         transcribed_audio: str = transcriber.transcribe(
             audio,
-            language=language)
+            language=language,
+            initial_prompt="Servy, Hey Servy, Hello Servy, Hi Servy, Please Servy, Hey Servy, Hi Servy, Hello Servy, Hey Survey, Hey Siri, Hey Server")
         print(transcribed_audio)
 
-def evaluate_audio(
-        sampling_rate: SamplingRate,
-        activation_window_size: int,
-        deactivation_window_size: int,
-        activation_threshold: float,
-        deactivation_threshold: float,
-        transcription_queue: Queue[bytes],
+def process_live_audio(
+        config_parser: ConfigParser,
+        audio_to_transcribe_queue: Queue[bytes],
         cancellation_event: threading.Event) -> None:
     """
-    Function to record audio using the VADRecorder.
+    Function to retrieve audio from the microphone using the VADRecorder, evaluate if there is speech,
+    and queue the audio for transcription. When the keyword is detected, it starts collecting the
+    audio servy command, transcribes it and publishes the resulting request.
+
     This function is intended to be run in a separate thread.
 
-    :param sampling_rate: SamplingRate enum value (LOW or HIGH)
-    :param activation_window_size: Size of the activation window in number of chunks
-    :param deactivation_window_size: Size of the deactivation window in number of chunks
-    :param activation_threshold: Threshold for activating audio collection
-    :param deactivation_threshold: Threshold for deactivating audio collection
-    :param transcription_queue: Queue to put audio data for transcription
+    :param config_parser: ConfigParser instance containing configuration settings
+    :param audio_to_transcribe_queue: Queue to put audio data for transcription
     :param cancellation_event: Event to signal cancellation of the recording process
     """
+    sampling_rate: SamplingRate = SamplingRate.from_string(config_parser.get("AUDIO", "sampling_rate", fallback="HIGH"))
+    activation_window_size: int = calculate_window_size(sampling_rate, config_parser.getint("VOICE_ACTIVATION", "activation_window_ms"))
+    deactivation_window_size: int = calculate_window_size(sampling_rate, config_parser.getint("VOICE_ACTIVATION", "deactivation_window_ms"))
+    activation_threshold: float = config_parser.getfloat("VOICE_ACTIVATION", "activation_threshold", fallback=0.9)
+    deactivation_threshold: float = config_parser.getfloat("VOICE_ACTIVATION", "deactivation_threshold", fallback=0.2)
+
     recorder: VADRecorder = VADRecorder(
         sampling_rate,
         parser.getint("AUDIO", "channels", fallback=1))
@@ -99,7 +100,7 @@ def evaluate_audio(
             print(
                 "Identified end of speech with confidence of {confidence}. Stop collection and queue for transcription...".format(
                     confidence=current_confidence))
-            transcription_queue.put(audio_to_transcribe)
+            audio_to_transcribe_queue.put(audio_to_transcribe)
             audio_to_transcribe = b''
             collecting_audio = False
 
@@ -114,29 +115,23 @@ if __name__ == "__main__":
     parser.read("../config/config.ini")
 
     queue: Queue = Queue(maxsize=0)
+    stop_event: threading.Event = threading.Event()
 
-    rate: SamplingRate = SamplingRate.from_string(parser.get("AUDIO", "sampling_rate", fallback="HIGH"))
-    act_window_size: int = calculate_window_size(rate, parser.getint("VOICE_ACTIVATION", "activation_window_ms"))
-    deact_window_size: int = calculate_window_size(rate, parser.getint("VOICE_ACTIVATION", "deactivation_window_ms"))
-    act_threshold: float = parser.getfloat("VOICE_ACTIVATION", "activation_threshold", fallback=0.9)
-    deact_threshold: float = parser.getfloat("VOICE_ACTIVATION", "deactivation_threshold", fallback=0.2)
-    num_transcriber_threads: int = parser.getint("TRANSCRIPTION", "number_of_transcriber_threads", fallback=1)
-    lan: str = parser.getint("TRANSCRIPTION", "language", fallback="en")
+    producer = Thread(target=process_live_audio, args=(parser, queue, stop_event))
+    producer.start()
+    threads = [producer]
 
-    trans: Transcriber = Transcriber(
+    audio_transcriber: Transcriber = Transcriber(
         model_name=parser.get("TRANSCRIPTION", "model_name", fallback="tiny"),
         device_type=parser.get("TRANSCRIPTION", "device_type", fallback="cpu"),
         number_of_threads=parser.getint("TRANSCRIPTION", "number_of_whisper_threads", fallback=1))
-
-    stop_event: threading.Event = threading.Event()
-    producer = Thread(target=evaluate_audio, args=(rate, act_window_size, deact_window_size, act_threshold, deact_threshold, queue, stop_event))
-    producer.start()
-
-    threads = [producer]
+    configured_language: str = parser.getint("TRANSCRIPTION", "language", fallback="en")
+    num_transcriber_threads: int = parser.getint("TRANSCRIPTION", "number_of_transcriber_threads", fallback=1)
     for _ in range(num_transcriber_threads):
-        transcriber_thread = Thread(target=transcribe_audio, args=(trans, lan, queue, stop_event))
+        transcriber_thread = Thread(target=transcribe_audio, args=(audio_transcriber, configured_language, queue, stop_event))
         threads.append(transcriber_thread)
         transcriber_thread.start()
+
     try:
         for thread in threads:
             thread.join()
